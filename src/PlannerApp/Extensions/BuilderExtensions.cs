ed402f8beeef13433dc649f7a00afae65f5a3f69
David Lopera <dlopera@greensqa.com>
2021-09-08 14:02:00 -0500

    Create the project & Initialize MudBlazor

---
+++ src/PlannerApp/Extensions/BuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using MudBlazor.Services;
+
+namespace PlannerApp.Extensions
+{
+  public static class BuilderExtensions
+  {
+    public static void ConfigureHttpClientBuilder(this WebAssemblyHostBuilder builder)
+    {
+      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    }
+
+    public static void ConfigureMudBlazor(this WebAssemblyHostBuilder builder)
+    {
+      builder.Services.AddMudServices();
+    }
+  }
+}
---
+++ src/PlannerApp/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using PlannerApp.Extensions;
+
+namespace PlannerApp
+{
+  public class Program
+  {
+    public static async Task Main(string[] args)
+    {
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("#app");
+
+      builder.ConfigureHttpClientBuilder();
+      builder.ConfigureMudBlazor();      
+
+      await builder.Build().RunAsync();
+    }
+  }
+}
