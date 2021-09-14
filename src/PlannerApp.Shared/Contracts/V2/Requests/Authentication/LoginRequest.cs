ff48f3a1ee1dd5ef227250c9be4dc81928d13516
David Lopera <dlopera@greensqa.com>
2021-09-13 19:57:11 -0500

    Implement the Auth Layout & Design the Login Form - Video Lesson 7

---
+++ src/PlannerApp.Shared/Contracts/V2/Requests/Authentication/LoginRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlannerApp.Shared.Contracts.V2.Requests.Authentication
+{
+  public class LoginRequest
+  {
+    [Required]
+    [EmailAddress]
+    [MaxLength(50)]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Password { get; set; }
+  }
+}
---
+++ src/PlannerApp/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+
+using PlannerApp.Shared.Contracts.V2.Requests.Authentication;
+
+namespace PlannerApp.Components
+{
+  public partial class LoginForm : ComponentBase
+  {
+    [Inject]
+    public HttpClient HttpClient { get; set; }
+
+    private LoginRequest _model = new();
+
+    private async Task LoginAsync()
+    {
+      throw new NotImplementedException(); 
+    }
+  }
+}
